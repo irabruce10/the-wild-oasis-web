f846b915d908558b754c09d8c68300cc8ddc781f
Bruce <irabruce10@gmail.com>
2024-08-30 15:37:14 +0200

    styling with tailwind

--- app/layout.js
+++ app/layout.js
@@ -8,7 +8,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className="bg-blue-900">
+      <body className="bg-primary-950 text-primary-100 min-h-screen">
         <header>
           <Logo />
         </header>
